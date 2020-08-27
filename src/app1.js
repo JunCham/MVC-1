9f478184d76e8a854e4ef5e70b61bba13f924149
JunCham <573373910@qq.com>
2020-08-27 18:07:37 +0800

    init之后add然后commit

---
+++ src/app1.js
@@ -0,0 +1,35 @@
+import $ from "jquery";
+import "./app1.css";
+
+const $button1 = $("#add");
+const $button2 = $("#minus");
+const $button3 = $("#mul");
+const $button4 = $("#divide");
+const $number = $("#number");
+
+const n = localStorage.getItem("n");
+$number.text(n || 100);
+$button1.on("click", () => {
+  let n = parseInt($number.text());
+  n += 1;
+  $number.text(n);
+  localStorage.setItem("n", n);
+});
+$button2.on("click", () => {
+  let n = parseInt($number.text());
+  n -= 1;
+  $number.text(n);
+  localStorage.setItem("n", n);
+});
+$button3.on("click", () => {
+  let n = parseInt($number.text());
+  n *= 2;
+  $number.text(n);
+  localStorage.setItem("n", n);
+});
+$button4.on("click", () => {
+  let n = parseInt($number.text());
+  n /= 2;
+  $number.text(n);
+  localStorage.setItem("n", n);
+});
